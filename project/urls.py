d1ff875060d1d3d5a88e5e1ec4d75bab59a7b3df
julio <julioaajr@gmail.com>
2020-04-21 19:22:56 -0300

    Conclusao da tela de login

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,13 @@
 from app import views
 from django.contrib import admin
 from django.urls import path
+from app import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',views.login_user),
+    path('logout/', views.logout_user),
+    path('login/submit', views.submit_login),
+    path('', views.index),
 ]
